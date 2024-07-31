1e5b516071e512214786fb9b51b06ee2cd3d57e7
bruno <brunofelippefaria@gmail.com>
2024-07-31 17:50:39 -0300

    loops

---
+++ L6 - loop while/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    //tabuada com while
+    int num;
+    std::cout << "digite um numero: ";
+    std::cin >> num;
+
+    int i = 0;
+
+    while (i < 10){
+        i++;
+        std::cout << num << " x " << i << " = " << num*i << std::endl;
+    }
+
+    return 0;
+}
